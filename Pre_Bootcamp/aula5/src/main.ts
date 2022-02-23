6873e8bfb93531f9f62aa110924db62ea2bd23e8
alexfarias7 <alexfarias44@gmail.com>
2022-02-23 15:25:37 -0300

    sending challenge 5

---
+++ Pre_Bootcamp/aula5/src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
+function sum (a:number,b:number){
+  return a+b
+}
+sum(1,2)
---
+++ desafios-pre-work-b-academy/dia5/app/src/http.ts
@@ -0,0 +1,32 @@
+
+type Methods = 'POST'|'DELETE'
+type DataPost ={
+  plate:string,
+  image: string,
+  brandModel:string,
+  year:number,
+  color: string
+}
+
+type DataDelete ={
+  plate:string
+}
+
+type Data = DataPost|DataDelete
+const request = (url:string, options?:RequestInit) =>
+  fetch(url, options)
+    .then(r => r.json())
+    .catch(e => ({ error: true, message: e.message }))
+
+
+const createRequest = (method:Methods) => <T>(url:string, data:T) => request(url, {
+  method,
+  headers: {
+    'content-type': 'application/json',
+  },
+  body: JSON.stringify(data)
+})
+
+export const get =  request
+export const post = createRequest('POST')
+export const del = createRequest('DELETE')
